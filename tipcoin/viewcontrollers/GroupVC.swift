c74a56060311d2e5931cdaaacf3b1bf99d25073e
yon <Lighthawky@gmail.com>
2015-07-01 16:48:22 +0300

    added group vc

--- tipcoin/viewcontrollers/GroupVC.swift
+++ tipcoin/viewcontrollers/GroupVC.swift
@@ -36,7 +36,7 @@ extension GroupViewController: UITableViewDataSource, UITableViewDelegate {
   }
   
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    return 3
   }
   
 }
